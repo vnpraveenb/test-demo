04445bf04619695975421038d659a07e247852da
root <root@localhost.localdomain>
2020-08-15 11:02:19 -0700

    my first commit of source code

---
+++ file2.java
@@ -0,0 +1,20 @@
+total 22
+lrwxrwxrwx.   1 root root    7 May 10  2019 bin -> usr/bin
+dr-xr-xr-x.   6 root root 1024 Aug 15 06:18 boot
+drwxr-xr-x.  19 root root 3260 Aug 15 04:39 dev
+drwxr-xr-x. 143 root root 8192 Aug 15 06:20 etc
+drwxr-xr-x.   3 root root   21 Aug 15 04:38 home
+lrwxrwxrwx.   1 root root    7 May 10  2019 lib -> usr/lib
+lrwxrwxrwx.   1 root root    9 May 10  2019 lib64 -> usr/lib64
+drwxr-xr-x.   2 root root    6 May 10  2019 media
+drwxr-xr-x.   3 root root   18 Aug 15 04:33 mnt
+drwxr-xr-x.   2 root root    6 May 10  2019 opt
+dr-xr-xr-x. 303 root root    0 Aug 15 04:39 proc
+dr-xr-x---.   3 root root  172 Aug 15 06:19 root
+drwxr-xr-x.  46 root root 1320 Aug 15 06:20 run
+lrwxrwxrwx.   1 root root    8 May 10  2019 sbin -> usr/sbin
+drwxr-xr-x.   2 root root    6 May 10  2019 srv
+dr-xr-xr-x.  13 root root    0 Aug 15 04:39 sys
+drwxrwxrwt.  20 root root 4096 Aug 15 07:28 tmp
+drwxr-xr-x.  12 root root  144 Aug 15 04:29 usr
+drwxr-xr-x.  23 root root 4096 Aug 15 06:20 var
